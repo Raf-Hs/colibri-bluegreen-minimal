e81f5ca518dd54add887146ecc9f9403c7b3f9c5
Rafael Silva <rafael.silva.06@outlook.com>
2025-11-23 21:09:13 -0600

    Cambios

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,17 @@
-import useVersion from "./useVersion";
-
 function App() {
-  const version = useVersion();
-
   return (
     <div style={{
-      background: version === "BLUE" ? "#3fa9f5" : "#7ed957",
+      width: "100vw",
+      height: "100vh",
+      backgroundColor: import.meta.env.VITE_COLOR,
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      fontSize: "3rem",
       color: "white",
-      padding: "20px",
-      fontSize: "20px",
-      textAlign: "center"
+      fontWeight: "bold"
     }}>
-      <h1>Ambiente activo: {version}</h1>
-
-      {/* CONTENIDO NORMAL DE LA APP */}
+      AMBIENTE: {import.meta.env.VITE_ENV}
     </div>
   );
 }
