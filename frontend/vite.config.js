d77d472940104204e52bd1310d1cba7bedead6ec
Rafael Silva <rafael.silva.06@outlook.com>
2025-11-23 21:32:44 -0600

    5

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
+
   return {
     plugins: [react()],
+
+    // 👇 LA LÍNEA QUE RESUELVE TU ERROR
+    base: "./",
+
     define: {
       "import.meta.env.VITE_ENVIRONMENT": JSON.stringify(env.VITE_ENVIRONMENT),
     },
